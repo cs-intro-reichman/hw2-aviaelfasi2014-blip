85b85692f0de63bd7c2e204a349bafaa5724bebb
aviaelfassy <aviaelfasi2014@gmail.com>
2025-11-12 21:41:01 +0200

    d

--- CalcPi.java
+++ CalcPi.java
@@ -1,8 +1,7 @@
 public class CalcPi {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         int N = Integer.parseInt(args[0]);
         double sum = 0.0;
-
         for (int i = 0; i < N; i++) {
             if (i % 2 == 0) {
                 sum += 1.0 / (2 * i + 1);
@@ -10,9 +9,8 @@ public class CalcPi {
                 sum -= 1.0 / (2 * i + 1);
             }
         }
-
         double valueOfPi = sum * 4;
-        System.out.println("Pi according to Java: " + Math.PI);
-        System.out.println("Pi, approximated:     " + valueOfPi);
-      }
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated:     " + valueOfPi);
+    }
 }
